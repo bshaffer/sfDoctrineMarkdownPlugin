db151253110049ab4776288cffc87475bd52b071
Brent Shaffer <bshafs@gmail.com>
2012-02-09 10:07:20 -0700

    adds configuration options for escaping HTML tags and entities

--- lib/parser/sfMarkdownParser.class.php
+++ lib/parser/sfMarkdownParser.class.php
@@ -5,6 +5,21 @@
 */
 class sfMarkdownParser extends MarkdownExtra_Parser
 {
+  protected $options;
+
+  public function __construct($options = array())
+  {
+    $this->options = array_merge(array(
+      'no_markup'   => sfConfig::get('app_sfDoctrineMarkdownPlugin_no_markup'),
+      'no_entities' => sfConfig::get('app_sfDoctrineMarkdownPlugin_no_entities'),
+    ), $options);
+    
+    $this->no_markup   = $this->options['no_markup'];
+    $this->no_entities = $this->options['no_entities'];
+
+    parent::__construct();
+  }
+  
   function doHardBreaks($text) {
     # Do hard breaks:
     return preg_replace_callback('/ {2,}\n|\n{1}/', 
